ec612e3ca9d61f32ff7691abd5609f23990e1d47
Igor Goulart <igorgoulartoliveira96@gmail.com>
2022-08-15 18:23:28 -0300

    routes configuration

--- app/src/main/java/goulart/composemvi/MainActivity.kt
+++ app/src/main/java/goulart/composemvi/MainActivity.kt
@@ -14,14 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeMVITheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    MainScreen()
-                }
-            }
+            MviApp()
         }
     }
 }
